ffa7c3fafe1262ae91c22184b0b04f0a3b88ff1c
Trinath9395 <trinathdevops0606@gmail.com>
2026-03-26 08:08:12 +0530

    python

--- 01-hello-world.py
+++ 01-hello-world.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+num1 = 10
+num2 = 5 
+
+addition = num1 + num2 
+print("value of addition -" + str(addition))
+
+sub = num1 - num2 
+print("value of sub -" - str(sub))
+
+nul = num1 * num2 
+print("value of the nul -" * str(nul))
+
