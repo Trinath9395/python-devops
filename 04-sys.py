0e819a46b37b8aefb8cf8d16e8873f2e038fc4b1
Trinath9395 <trinathdevops0606@gmail.com>
2026-03-27 10:53:54 +0530

    python

--- 04-sys.py
+++ 04-sys.py
@@ -11,9 +11,9 @@ def mul(num1, num2):
     mul = num1 * num2 
     return mul 
 
-num1 = sys.argv[1]
+num1 = int(sys.argv[1])
 operation = sys.argv[2]
-num2 = sys.argv[3]
+num2 = int(sys.argv[3])
 
 if operation == "add":
     output = add(num1, num2)
